4eaa9764d9faea29fad0c0d71d6a8873ace3920d
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-18 00:23:00 +0700

    add modal dialog login & register

--- client/script/script.js
+++ client/script/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-
+  // //login
+  // $('#login').on('click', function(e){
+  //   e.preventDefault()
+  //   alert()
+  //   showModalLogin()
+  // })
+  //
+  // //register
+  // $('#register').on('click', function(e){
+  //   e.preventDefault()
+  //   alert()
+  //   showModalRegister()
+  // })
 })
